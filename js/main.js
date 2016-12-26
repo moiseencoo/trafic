9644633de2080fdbc0bf350b567f797b32bd9a10
moiseencoo <95varya@mail.ru>
2016-12-26 18:58:53 +0500

    bueh

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-/*
-$( document ).ready(function(){
+$('ul.ul-news').each(function(){
+          var $active, $content, $links = $(this).find('a');
 
-	
-	AOS.init({
-		
-	});
+          $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+          $active.addClass('active');
 
-});*/
+          $content = $($active[0].hash);
+
+          // Hide the remaining content
+          $links.not($active).each(function () {
+            $(this.hash).hide();
+          });
+
+          $(this).on('click', 'a', function(e){
+            $active.removeClass('active');
+            $content.hide();
+
+            $active = $(this);
+            $content = $(this.hash);
+
+            $active.addClass('active');
+            $content.show();
+
+            e.preventDefault();
+          });
+        });
